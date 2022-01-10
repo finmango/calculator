0933f6663f32a89b47b360fde811e82b7c4a894e
Oscar Wahltinez <oscar@wahltinez.org>
2022-01-10 16:10:15 -0500

    Array.prototype.at

--- src/static/polyfills.js
+++ src/static/polyfills.js
@@ -50,4 +50,11 @@
         return this;
     };
 
+    Array.prototype.at = function at(n) {
+        n = Math.trunc(n) || 0;
+        if (n < 0) n += this.length;
+        if (n < 0 || n >= this.length) return undefined;
+        return this[n];
+    };
+
 })();
